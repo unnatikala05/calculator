908ae6487e662a7ab434a2c3b5d364b5badcea05
unnatikala05 <53338821+unnatikala05@users.noreply.github.com>
2019-10-01 12:20:04 +0530

    update react redux

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import ReactDOM from "react-dom";
 import App from "./component/App";
 import "./index.css";
 import "github-fork-ribbon-css/gh-fork-ribbon.css";
-
+import  React from 'react-redux';
 ReactDOM.render(<App />, document.getElementById("root"));
